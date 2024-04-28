41ccde1200824bedbe15ee94bdd629d0d704fe95
aidanho1188 <aaron23657@gmail.com>
2024-04-28 16:00:46 -0400

    Add favicon

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -10,7 +10,7 @@ import page from '../page'
 export default function NavMenu() {
   return (
     <div className='relative w-full flex items-center justify-center'>
-      <Navbar className='top-2' />
+      <Navbar className='top-0 ' />
     </div>
   )
 }
@@ -18,9 +18,9 @@ export default function NavMenu() {
 function Navbar({className}: {className?: string}) {
   const [active, setActive] = useState<string | null>(null)
   return (
-    <div className={cn('fixed top-10 inset-x-0 max-w-2xl mx-auto z-50', className)}>
+    <div className={cn('fixed top-0 inset-x-0 w-full mx-auto z-50', className)}>
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item='Services'>
+        <MenuItem setActive={setActive} active={active} item='Home'>
           <div className='flex flex-col space-y-4 text-sm'>
             <HoveredLink href='/web-dev'>Web Development</HoveredLink>
             <HoveredLink href='/interface-design'>Interface Design</HoveredLink>
@@ -28,7 +28,7 @@ function Navbar({className}: {className?: string}) {
             <HoveredLink href='/branding'>Branding</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item='Products'>
+        <MenuItem setActive={setActive} active={active} item='About'>
           {/* <div className='  text-sm grid grid-cols-2 gap-10 p-4'>
             <ProductItem title='Algochurn' href='https://algochurn.com' src='https://assets.aceternity.com/demos/algochurn.webp' description='Prepare for tech interviews like never before.' />
             <ProductItem title='Tailwind Master Kit' href='https://tailwindmasterkit.com' src='https://assets.aceternity.com/demos/tailwindmasterkit.webp' description='Production ready Tailwind css components for your next project' />
@@ -36,7 +36,7 @@ function Navbar({className}: {className?: string}) {
             <ProductItem title='Rogue' href='https://userogue.com' src='https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png' description='Respond to government RFPs, RFIs and RFQs 10x faster using AI' />
           </div> */}
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item='Pricing'>
+        <MenuItem setActive={setActive} active={active} item='Contact Me'>
           <div className='flex flex-col space-y-4 text-sm'>
             <HoveredLink href='/hobby'>Hobby</HoveredLink>
             <HoveredLink href='/individual'>Individual</HoveredLink>
