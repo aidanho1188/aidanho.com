91b344f3fadde1d40139cd4acfa60e15e215cfef
aidanho1188 <aaron23657@gmail.com>
2024-05-10 01:42:05 -0400

    clean the rest of home page css

--- src/app/home/components/Projects.js
+++ src/app/home/components/Projects.js
@@ -1,11 +1,13 @@
+import styles from '../styles/Projects.module.css'
+
 export default function Projects({data}) {
   return (
-    <div className='flex flex-col items-center justify-center mx-4'>
-      {data.projects.map((project) => (
-        <div key={project.id} className='flex flex-col items-center justify-center mx-4'>
-          <p>{project.name}</p>
-          <p>{project.description}</p>
-          <p>{project.duration}</p>
+    <div className={styles.containter}>
+      {data.projects.map(({id, name, description, duration}) => (
+        <div key={id} className={styles.card}>
+          <p>{name}</p>
+          <p>{description}</p>
+          <p>{duration}</p>
         </div>
       ))}
     </div>
