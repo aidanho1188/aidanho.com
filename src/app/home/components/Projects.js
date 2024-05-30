e49a649c1162af21ba697964e665cbfa2e4b9b38
aidanho1188 <aaron23657@gmail.com>
2024-05-30 17:47:28 -0400

    update animation to each elements in map

--- src/app/home/components/Projects.js
+++ src/app/home/components/Projects.js
@@ -2,9 +2,9 @@ import styles from '../styles/Projects.module.css'
 
 export default function Projects({data}) {
   return (
-    <div data-aos='zoom-in' className={styles.container}>
+    <div className={styles.container}>
       {data.map(({id, name, description, duration, link}) => (
-        <a key={id} className={styles.card} href={link}>
+        <a data-aos='zoom-in' key={id} className={styles.card} href={link}>
           <div>Icon</div>
           <div className={styles.contents}>
             <p>{name}</p>
