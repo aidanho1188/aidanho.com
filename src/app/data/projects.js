b1b4144e48f97dc295f3aab8702db2355893087c
aidanho1188 <aaron23657@gmail.com>
2024-06-03 18:42:58 -0400

    refactor: Update project data with more descriptive information

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -1,27 +1,27 @@
 const projects = [
   {
     id: 1,
-    name: 'Project name',
-    description: 'Project description...',
-    link: '#',
+    name: 'React Chat App',
+    description: 'Web-based real-time chat application with user sign-up and login and multiple chat rooms. Used Express, Postman and MongoDB to build and test server CRUD functionality. Built custom client side using React, Reactrap and CSS.',
+    link: 'https://github.com/uprighted-learners/g3-react-chat',
   },
   {
     id: 2,
-    name: 'Project name',
-    description: 'Project description...',
-    link: '#',
+    name: 'Zorkington Adventure Game',
+    description: 'A custom adventure text-based game. Created purely with JavaScript using state machine design to represent potential room transitions.',
+    link: 'https://github.com/burlingtoncodeacademy-students/zorkington-aidanho1188/tree/master',
   },
   {
     id: 3,
-    name: 'Project name',
-    description: 'Project description...',
-    link: '#',
+    name: 'Interactive Jeopardy Game',
+    description: 'Custom web-based Two-player Jeopardy game with two rounds and a final Jeopardy. Created from scratch using HTML, CSS, and vanilla JavaScript.',
+    link: 'https://github.com/burlingtoncodeacademy-students/jeopardy-board-aidanho1188',
   },
   {
     id: 4,
-    name: 'Project name',
-    description: 'Project description...',
-    link: '#',
+    name: 'RPN Calculator',
+    description: 'Reverse Polish Notation (RPN or stack) calculator in which the operator symbol is placed after the arguments being operated on. ',
+    link: 'https://github.com/USM-COS470-S21/rpn-calculator-aidan-ho-solo',
   },
 ]
 
