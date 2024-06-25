4b1abbc54e794a806b65fb396efc64efe90a4309
aidanho1188 <aaron23657@gmail.com>
2024-06-25 12:21:59 -0400

    refactor: Update contact information with LinkedIn, GitHub, and Discord links

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -16,19 +16,28 @@ export default function Contact() {
           <div className={styles.right}>
             <h3 className='text-2xl font-bold m-5'>Contact Information</h3>
             <p className='m-5'>
-              Email: <a href='mailto:aidanho23657@gmail.com'></a>
+              Email:{' '}
+              <a href='mailto:aidanho23657@gmail.com' target='_blank'>
+                aidanho23657@gmail.com
+              </a>
             </p>
             <p className='m-5'>
-              Phone: <a href='tel:1234567890'>123-456-7890</a>
+              LinkedIn:{' '}
+              <a href='https://www.linkedin.com/in/aidan-ho' target='_blank'>
+                https://www.linkedin.com/in/aidan-ho
+              </a>
             </p>
             <p className='m-5'>
-              LinkedIn: <a href='https://www.linkedin.com/in/your-linkedin-profile'>Your LinkedIn Profile</a>
+              GitHub:{' '}
+              <a href='https://github.com/aidanho1188' target='_blank'>
+                https://github.com/aidanho1188
+              </a>
             </p>
             <p className='m-5'>
-              GitHub: <a href=''>Github</a>
-            </p>
-            <p className='m-5'>
-              Discord: <a href=''>Discord</a>
+              Discord:{' '}
+              <a href='https://discord.com/users/1172334442702983202' target='_blank'>
+                aidanho23657
+              </a>
             </p>
           </div>
         </div>
