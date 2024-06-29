eec62dd3d655454144c27858f0843a5e6e7a5a8c
aidanho1188 <aaron23657@gmail.com>
2024-06-29 00:52:20 -0400

    refactor: Add Zorkington V2 to currentProjects.js

--- src/app/data/currentProjects.js
+++ src/app/data/currentProjects.js
@@ -6,6 +6,13 @@ const currentProjects = [
     duration: 'May 2024 - Present',
     link: 'https://github.com/aidanho1188/aidanho.com',
   },
+  {
+    id: 2,
+    title: 'Zorkington V2',
+    description: 'Zorkington V2 is a text-based adventure game that I am building. It is inspired by the original Zork game.  It will have a command-line interface web app. It will be deployed on AWS Lambda and API Gateway',
+    duration: 'July 2024 - Present',
+    link: '',
+  },
 ]
 
 export default currentProjects
