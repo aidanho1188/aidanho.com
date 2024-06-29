4e030327d85604f0a586271c7cd48408f18e2944
aidanho1188 <aaron23657@gmail.com>
2024-06-29 00:46:52 -0400

    refactor: Update personal website description with more details

--- src/app/data/currentProjects.js
+++ src/app/data/currentProjects.js
@@ -2,7 +2,7 @@ const currentProjects = [
   {
     id: 1,
     title: 'Personal Website',
-    description: 'This is my personal website. It is built with Next.js,Tailwind CSS, and Aceternity UI.',
+    description: 'This is my personal website. It is built with Next.js, Tailwind CSS, and Aceternity UI. It is hosted on Vercel. I use this website to showcase my projects and blog about my experiences in software development.',
     duration: 'May 2024 - Present',
     link: 'https://github.com/aidanho1188/aidanho.com',
   },
