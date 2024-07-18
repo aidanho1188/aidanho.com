3b034ad468c1d9c05ee24e6654e39752003da5d1
aidanho1188 <aaron23657@gmail.com>
2024-07-18 14:26:32 -0400

    refactor: Update project cards in Projects.module.css

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -12,7 +12,7 @@ const projects = [
     title: 'Zorkington Adventure Game ',
     description: 'A custom adventure text-based game. Created purely with JavaScript using state machine design to represent potential room transitions.',
     duration: 'Oct 2023 - Dec 2023',
-    link: 'https://github.com/burlingtoncodeacademy-students/zorkington-aidanho1188/tree/master',
+    link: 'https://github.com/uprighted-learners/zorkington-aidanho1188/releases/tag/v1.0',
     currentProjects: false,
   },
   {
@@ -44,7 +44,7 @@ const projects = [
     title: 'Zorkington V2',
     description: 'Zorkington V2 is a text-based adventure game that I am building. It is a continuation of the original Zorkington game that I built during my time at Upright Education. I am building this game to learn more about game development and to improve my programming skills. I am using JavaScript and Node.js to build this game. I am also using Git and GitHub to manage the project and deploy it to a static webpage.',
     duration: 'July 2024 - Present',
-    link: '',
+    link: 'https://uprighted-learners.github.io/zorkington-aidanho1188/',
     currentProjects: true,
   },
 ]
